e31bb725ef00c6e492301794dfe893f24d13e0f3
Jeong Jae-Seong <gik1819@gmail.com>
2023-08-14 16:23:36 +0900

    :tada: 기본 세팅 완료

---
+++ src/app/sandbox/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import "./sandbox.scss";
+
+export default function Sandbox() {
+  return (
+    <main className="sandbox-screen">
+      <Link href="/">HIHIHI</Link>
+    </main>
+  );
+}
