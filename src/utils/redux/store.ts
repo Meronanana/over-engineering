ab649ab18f16e7509f5735283615056280bb5555
Jae-Seong Jeong <gik1819@gmail.com>
2023-11-17 21:54:37 +0900

    :bug: 쓰이지 않는 reducer 정리

--- src/utils/redux/store.ts
+++ src/utils/redux/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import modalStateReducer from "./modalState";
-import netselStateReducer from "./netselState";
 
 export const rootReducer = combineReducers({
   modalStateReducer,
-  netselStateReducer,
 });
 
 export const store = configureStore({
