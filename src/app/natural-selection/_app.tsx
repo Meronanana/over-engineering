33f77ab74718fc5d84985a6061d7841ffbdbfc6e
Jae-Seong Jeong <gik1819@gmail.com>
2023-11-17 16:58:07 +0900

    :sparkles: Map 마우스로 이동하는 기능 구현

--- src/app/natural-selection/_app.tsx
+++ src/app/natural-selection/_app.tsx
@@ -20,6 +20,7 @@ import {
 } from "./model/constants";
 import CreatureDataInspector from "./controller/CreatureDataInspector";
 import { MapPosition } from "./model/types";
+import { ScreenCoordinate } from "@/utils/physicalEngine";
 
 export default function NaturalSelection() {
   const flatTileRefs = useRef<TileRef<FlatTileType>[][]>([]);
@@ -30,20 +31,23 @@ export default function NaturalSelection() {
 
   const sizeIndexRef = useRef<number>(0); // 0: 16, 1: 32, 2: 64
 
+  const mouseDownRef = useRef<boolean>(false);
+  const camPosRef = useRef<ScreenCoordinate>({ X: 0, Y: 0 });
+
   useEffect(() => {
-    initIndex();
-    window.addEventListener("resize", initIndex);
+    initPage();
+    window.addEventListener("resize", initPage);
     // setIndex();
     flatTileRefs.current = createInitFlatTileRefs();
     creatureRefs.current = createInitCreatureRefs();
     foodRefs.current = createInitFoodRefs();
 
     return () => {
-      window.removeEventListener("resize", initIndex);
+      window.removeEventListener("resize", initPage);
     };
   }, []);
 
-  const initIndex = () => {
+  const initPage = () => {
     if (window.innerHeight > window.innerWidth) {
       if (window.innerHeight > 1280) {
         sizeIndexRef.current = 2;
@@ -68,17 +72,50 @@ export default function NaturalSelection() {
     document.documentElement.style.setProperty("--creature-size", `${CREATURE_SIZE[sizeIndexRef.current]}px`);
   };
 
+  const mouseDownEvent = (e: React.MouseEvent) => {
+    mouseDownRef.current = true;
+  };
+
+  const mouseUpEvent = (e: React.MouseEvent) => {
+    mouseDownRef.current = false;
+  };
+
+  const mouseMoveEvent = (e: React.MouseEvent) => {
+    if (!mouseDownRef.current) return;
+
+    const newX = camPosRef.current.X + e.movementX;
+    const newY = camPosRef.current.Y + e.movementY;
+    const limitX = MAP_SIZE * TILE_SIZE[sizeIndexRef.current] - window.innerWidth;
+    const limitY = MAP_SIZE * TILE_SIZE[sizeIndexRef.current] - window.innerHeight;
+
+    if (newX < 0) camPosRef.current.X = 0;
+    else if (newX > limitX) camPosRef.current.X = limitX;
+    else camPosRef.current.X = newX;
+
+    if (newY < 0) camPosRef.current.Y = 0;
+    else if (newY > limitY) camPosRef.current.Y = limitY;
+    else camPosRef.current.Y = newY;
+
+    console.log(camPosRef.current);
+  };
+
   return (
     <main>
-      <div className="natsel-screen">
+      <div className="natsel-screen" onMouseDown={mouseDownEvent} onMouseUp={mouseUpEvent} onMouseMove={mouseMoveEvent}>
         <MapController
           flatTileRefs={flatTileRefs}
           aboveDecoRefs={aboveDecoRefs}
           overDecoRefs={overDecoRefs}
           sizeIndex={sizeIndexRef}
+          camPosRef={camPosRef}
         />
-        <FoodController foodRefs={foodRefs} sizeIndex={sizeIndexRef} />
-        <CreatureController creatureRefs={creatureRefs} foodRefs={foodRefs} sizeIndex={sizeIndexRef} />
+        <FoodController foodRefs={foodRefs} sizeIndex={sizeIndexRef} camPosRef={camPosRef} />
+        <CreatureController
+          creatureRefs={creatureRefs}
+          foodRefs={foodRefs}
+          sizeIndex={sizeIndexRef}
+          camPosRef={camPosRef}
+        />
         <CreatureDataInspector creatureRefs={creatureRefs} />
       </div>
     </main>
